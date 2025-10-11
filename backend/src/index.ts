15dea7bde8ccf37d473bddd2adfd4e856ff81e72
Kirill Proshchenkov <kirill.proshchenkov@naumen.ru>
2025-10-11 21:25:36 +0300

    fix backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,39 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { setupSwagger } from "./lib/swagger";
-import { authRouter } from "./routes/auth.routes";
-import { genresRouter } from "./routes/genres.routes";
-import { errorHandler } from "./middleware/error.middleware";
-import { authorsRouter } from "./routes/author.routes";
-dotenv.config();
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { setupSwagger } from './lib/swagger'
+import { authRouter } from './routes/auth.routes'
+import { genresRouter } from './routes/genres.routes'
+import { errorHandler } from './middleware/error.middleware'
+import { authorsRouter } from './routes/author.routes'
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
 const corsOptions = {
-  origin: "*",
+  origin: '*',
   optionsSuccessStatus: 200,
   credentials: true,
-};
+}
 
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/uploads", express.static("uploads"));
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/uploads', express.static('uploads'))
 
-setupSwagger(app);
+setupSwagger(app)
 
-app.use("/api/auth", authRouter);
-app.use(errorHandler);
+app.use(errorHandler)
 
-app.use("/api/genres", genresRouter);
-
-app.use("/api/authors", authorsRouter);
+app.use('/api/auth', authRouter)
+app.use('/api/genres', genresRouter)
+app.use('/api/authors', authorsRouter)
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/api`)
   console.log(
-    `Swagger documentation available at http://localhost:${port}/api-docs`
-  );
-});
+    `Swagger documentation available at http://localhost:${port}/api-docs`,
+  )
+})
